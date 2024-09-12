bac53865d9a5acc508670f9885202120a8808c78
joao <joao.23204708360001@faeterj-rio.edu.br>
2024-09-12 10:01:49 -0300

    l3 terminada

--- AULA_2/Lista_exercicio_3/Exercicio10.java
+++ AULA_2/Lista_exercicio_3/Exercicio10.java
@@ -1,3 +1,8 @@
+/*
+    10. Elabore um programa que leia diversos números e ao final escreva: a quantidade de números 
+    digitados, a quantidade de números pares, a quantidade de números ímpares.
+*/
+
 package AULA_2.Lista_exercicio_3;
 import java.util.Scanner;
 
@@ -5,6 +10,34 @@ public class Exercicio10 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in))
         {
+            boolean cond = true;
+            int par = 0;
+            int impar = 0;
+            
+            while (cond)
+            {
+                System.out.print("Insira um valor: ");
+                int num = scanner.nextInt();
+
+                if ((num == 0))
+                { 
+                    cond = false;
+                }else{
+                    if ((num % 2) == 0)
+                    {
+                        par += 1;
+                    }else{
+                        impar += 1;
+                    }
+                }
+            }
+
+            int total = par + impar;
+
+            System.out.println("par: " + par);
+            System.out.println("impar: " + impar);
+            System.out.println("Total: " + total);
+
             scanner.close();
         }
     }
--- AULA_2/Lista_exercicio_3/Exercicio8.java
+++ AULA_2/Lista_exercicio_3/Exercicio8.java
@@ -1,3 +1,8 @@
+/* 
+    8.  Crie um programa que leia o nome e a altura de 10 pessoas e ao final escreva: 
+    a altura média do grupo, o nome e a altura da pessoa mais alta.
+*/
+
 package AULA_2.Lista_exercicio_3;
 import java.util.Scanner;
 
@@ -5,6 +10,32 @@ public class Exercicio8 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in))
         {
+            int len = 10;
+            String nomes[] = new String[len];
+            double altura = 0;
+
+            int indNomes = 0;
+            double maior = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                System.out.print("Insira seu nome: ");
+                nomes[i] = scanner.nextLine();
+                System.out.print("Insira sua altura: ");
+                altura = scanner.nextDouble();
+
+                scanner.nextLine(); //Consumir a quebra de linha após nextDouble()
+                System.out.println();
+
+                if (altura > maior){
+                    maior = altura;
+                    indNomes = i;
+                }
+
+            }
+
+            System.out.println(nomes[indNomes] + " e o mais alto com " + altura + " de altura");
+
             scanner.close();
         }
     }
